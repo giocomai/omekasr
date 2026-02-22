c5d1e8aaaeecd8e7abd1c63cd9b9ed7ddc8b6042
Giorgio Comai <g@giorgiocomai.eu>
2026-02-22 20:48:34 +0100

    retrieve property

--- R/or_get_property.R
+++ R/or_get_property.R
@@ -1,6 +1,6 @@
-#' Retrieve Omeka S item based on id
+#' Retrieve Omeka S property based on id
 #'
-#' @param item_id The numeric identifier of an Omeka S item.
+#' @param property_id The numeric identifier of an Omeka S property.
 #' @param base_url Defaults to `NULL`, typically set for the active session with
 #'   [or_set()].
 #'
@@ -9,20 +9,20 @@
 #'
 #' @examples
 #' \dontrun{
-#'   or_get_item(id = 123)
+#'   or_get_property(id = 12)
 #' }
-or_get_item <- function(item_id, base_url = NULL) {
+or_get_property <- function(property_id, base_url = NULL) {
   if (is.null(base_url)) {
     base_url <- or_set()[["base_url"]]
   }
 
   req <- httr2::request(base_url) |>
-    httr2::req_url_path_append("items", as.character(item_id))
+    httr2::req_url_path_append("properties", as.character(property_id))
 
   resp <- req |>
     httr2::req_perform()
 
-  item_l <- httr2::resp_body_json(resp)
+  property_l <- httr2::resp_body_json(resp)
 
-  item_l
+  property_l
 }
