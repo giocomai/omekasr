1f7977ffe32167b907ab53f657653c59fffeacc3
Giorgio Comai <g@giorgiocomai.eu>
2026-03-15 21:24:29 +0100

    check url validity in o_set

--- R/o_set.R
+++ R/o_set.R
@@ -3,28 +3,42 @@
 #' @param base_url Endpoint of the API of the Omeka installation, or base url of
 #'   the website. Internally, it is ensured that the `api` suffix is always
 #'   appended to the given url.
+#' @param error Defaults to `TRUE`. Passed to [o_check_url()]. If `TRUE`, throws
+#'   an error if the url set or retrieved is invalid.
 #'
 #' @returns Invisibly, the settings as a list.
 #' @export
 #'
 #' @examples
 #'
-#'o_set(base_url = "https://example.com/api")
+#' o_set(base_url = "https://example.com/api")
 #'
-#'base_url <- o_set()[["base_url"]]
+#' base_url <- o_set()[["base_url"]]
 #'
-#'base_url
-o_set <- function(base_url = NULL) {
+#' base_url
+o_set <- function(base_url = NULL, error = TRUE) {
   if (is.null(base_url)) {
     base_url <- Sys.getenv("omekasr_base_url")
   } else {
     Sys.setenv(omekasr_base_url = as.character(base_url))
   }
+
+  if (base_url == "") {
+    cli::cli_abort(
+      message = c(
+        "x" = "The provided {.arg base_url} is empty.",
+        "!" = "Ensure a valid {.arg base_url} has been provided, or set with {.fun o_set}."
+      )
+    )
+  }
+
   if (!stringr::str_ends(string = base_url, pattern = "/api|/api/")) {
     base_url <- fs::path(base_url, "api")
   } else if (stringr::str_ends(string = base_url, pattern = "/api/")) {
     base_url <- stringr::str_remove(string = base_url, pattern = "/$")
   }
+
+  o_check_url(base_url = base_url, silent = TRUE, error = error)
 
   invisible(list(
     base_url = as.character(base_url)
--- tests/testthat/test-o_set.R
+++ tests/testthat/test-o_set.R
@@ -1,4 +1,6 @@
+library("omekasr")
 library("testthat")
+
 test_that("API is appended correctly to the base url", {
   expect_equal(
     o_set("https:/example.com/")[["base_url"]],
@@ -19,4 +21,15 @@ test_that("API is appended correctly to the base url", {
     o_set("https:/example.com")[["base_url"]],
     "https:/example.com/api"
   )
+})
+
+
+test_that("An error is thrown is an invalid url is given", {
+  expect_error({
+    settings <- o_set(base_url = "")
+  })
+
+  expect_no_error({
+    settings <- o_set(base_url = "https:/example.com")
+  })
 })
